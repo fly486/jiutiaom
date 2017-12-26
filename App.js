4e8c3a4b55b06142cb55fd0a4d63796cf52d02eb
an <an10101@163.com>
2017-12-26 23:17:08 +0800

    mabx

--- App.js
+++ App.js
@@ -11,26 +11,27 @@ import {
   Text,
   View
 } from 'react-native';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react/native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component<{}> {
+@observer
+export default class App extends Component {
+
+  @observable
+  a = 1
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, aedit App.js
+        <Text onPress = {()=>this.a++} style={styles.instructions}>
+          {this.a}
         </Text>
         <Text style={styles.instructioans}>
-          {instructions}
+         
         </Text>
       </View>
     );
